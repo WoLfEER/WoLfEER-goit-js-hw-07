5044d096e490908d63b5db5a2e9a5fe182f00b7d
Andrew Vovk <101401666+WoLfEER@users.noreply.github.com>
2022-08-02 16:18:16 +0300

    asd

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryMarkup = imageMarkup(galleryItems);
+const imageContainer = document.querySelector('.gallery');
+
+
+imageContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+function imageMarkup(galleryItems) {
+        return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>`
+    }).join('');
+}
+
+imageContainer.addEventListener('click',(event) => {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a',
+    {captionsData : 'alt',
+     captionDelay : 250,
+    //  loop : false,
+    //  enableKeyboard :false,
+    });
+})
